6e7d9e5a594af77f5056ac64805fbff020fc273e
Felix Seip <felixseip01@gmail.com>
2021-02-18 23:05:57 +0100

    Added form

---
+++ src/components/my-form.js
@@ -0,0 +1,39 @@
+import { Form, Input, Row, Col, FormGroup, Label, Button } from "reactstrap";
+
+function MyForm(props) {
+  return (
+    <div className="row">
+      <section className="py-5 px-5 mx-auto">
+        <h1 className="hero-heading mb-0">{props.data.title}</h1>
+        <p className="text-muted mb-5">{props.data.subtitle}</p>
+        <Form>
+          {props.data.items.map((item) => (
+            <>
+              {(item.type === "text" ||
+                item.type === "email" ||
+                item.type === "password") && (
+                <Col className="form-group">
+                  <FormGroup>
+                    <Label for="phone" className="form-label">
+                      {item.title}
+                    </Label>
+                    <Input type={item.type} required={item.required} />
+                  </FormGroup>
+                </Col>
+              )}
+              {item.type === "button" && (
+                <Col className="form-group">
+                  <FormGroup>
+                    <Button type="submit">{item.title}</Button>
+                  </FormGroup>
+                </Col>
+              )}
+            </>
+          ))}
+        </Form>
+      </section>
+    </div>
+  );
+}
+
+export default MyForm;
